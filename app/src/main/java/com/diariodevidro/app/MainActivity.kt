cad6800d0c71119dd86aaad5ac0153a8251f00d9
jessycaslimajsl-hue <jessycaslimajsl@gmail.com>
2026-09-21 14:31:33 -0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/diariodevidro/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.diariodevidro.app
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebSettings
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+ override fun onCreate(s: Bundle?) {
+  super.onCreate(s)
+  val w = WebView(this)
+  w.settings.javaScriptEnabled = true
+  w.settings.domStorageEnabled = true
+  w.loadUrl("file:///android_asset/index.html")
+  setContentView(w)
+ }
+}
